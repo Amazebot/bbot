3a9dd93a1b238368f1e2ad4764efb49c87e0dc42
Tim Kinnane <timkinnane@gmail.com>
2018-05-15 15:41:33 +1000

    docs(demo): Add listener types demo script
    Demo scripts provide new pattern for coding examples, will be expanded with cli to perform demos.

---
+++ src/demo/listen-types.ts
@@ -0,0 +1,43 @@
+import * as bot from '..'
+/** @todo Disable file logging in demo */
+
+/**
+ * Setting up basic listeners.
+ * Run demo using `ts-node src/demo/listen-types`
+ * Shows different matching methods to trigger the same bit.
+ */
+
+// Sends flowers to the console...
+bot.setupBit({
+  id: 'send-flowers',
+  callback: (b: bot.B) => bot.logger.info(`💐  - ${b.listener.id} matched on "${b.message.toString()}"`)
+})
+
+// ...whenever someone says flowers
+bot.listenText(/flowers?/i, 'send-flowers', { id: 'text-listener' })
+
+// ...or mentions a known flower
+const flowers = ['rose', 'tulip', 'orchid', 'iris', 'peony', 'magnolia']
+bot.listenCustom((message: bot.TextMessage) => {
+  return flowers.find((f) => message.text.indexOf(f) !== -1)
+}, 'send-flowers', { id: 'custom-listener' })
+
+// ...or a custom request returns positive
+const flowerLookup = async (input: any) => {
+  await new Promise((resolve) => setTimeout(resolve, 500))
+  return input
+}
+bot.listenCustom((message: bot.TextMessage) => {
+  return flowerLookup(message)
+}, 'send-flowers', { id: 'custom-listener-async' })
+
+// Process demo inputs
+const user = new bot.User()
+const start = async () => {
+  await bot.start()
+  await bot.receive(new bot.TextMessage(user, 'flowers please'))
+  await bot.receive(new bot.TextMessage(user, '2 orchids for me'))
+  await bot.receive(new bot.TextMessage(user, 'I <3 flowers'))
+}
+
+start().catch((err) => bot.logger.error(err))
